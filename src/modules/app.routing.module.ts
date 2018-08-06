adefffe449e1db1aeaf37833abebd2d942002bcd
Chris Ray <CRay@theambitgroup.com>
2018-08-06 17:23:40 -0400

    Education Module and Css

--- src/modules/app.routing.module.ts
+++ src/modules/app.routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { GranteeComponent } from '../components/grantee/grantee.component'
 import { GranteeChartComponent } from '../components/grantee-chart/grantee-chart.component';
 import { GranteeTransactionsComponent } from '../components/grantee-transactions/grantee-transactions.component';
-
+import { EducationComponent } from '../components/education/education.component'
 
 import { GranteeService } from '../services/grantee.service'
 
@@ -13,7 +13,7 @@ const routes: Routes = [
     path: 'grantee',
     component: GranteeComponent
   },
-  { path: 'education', component: GranteeChartComponent },
+  { path: 'education', component: EducationComponent },
   { path: '', redirectTo: 'grantee', pathMatch: 'full' }
 ]
 

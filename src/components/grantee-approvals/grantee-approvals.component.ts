edcdadd29cd7aef0727956c230357548201fe8e6
Bryan Tanifum (WinXDev4-Ubuntu) <bryan.tanifum@gmail.com>
2018-08-29 15:56:50 -0400

    Added functionality for the grantee approval component

--- src/components/grantee-approvals/grantee-approvals.component.ts
+++ src/components/grantee-approvals/grantee-approvals.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {MatDialog} from '@angular/material';
 import { ApprovalDialogComponent } from '../dialogs/approval/approval.dialog.component';
+import { GrantBlockService } from '../../services/grantblock.service';
+import { Transactions } from '../../models/transactions.model';
 
 @Component({
   selector: 'grantee-approvals',
@@ -10,10 +12,25 @@ import { ApprovalDialogComponent } from '../dialogs/approval/approval.dialog.com
 export class GranteeApprovalsComponent implements OnInit {
 
   constructor(
-    public dialog : MatDialog
+    public dialog : MatDialog,
+    public $grantblockService : GrantBlockService
   ) { }
 
+  granteeId: string;
+  myApprovals: Transactions[];
+  @Input() set SetGranteeId(_granteeId:string){
+    this.granteeId = _granteeId;
+    console.log(this.granteeId);
+    if(this.granteeId){
+      this.$grantblockService.GetGranteeApprovals(this.granteeId).subscribe((_myApprovals)=>{
+        this.myApprovals = _myApprovals;
+        console.log(this.myApprovals);
+      })
+    }
+  };
+
   ngOnInit() {
+
   }
 
   openDialog(): void {

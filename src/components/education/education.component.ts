d33bdff7eaa6c66385fcf78fb51878b2751c45b9
Chris Ray <CRay@theambitgroup.com>
2018-08-30 14:58:48 -0400

    UI/UX updates to Education section to match grantee section

--- src/components/education/education.component.ts
+++ src/components/education/education.component.ts
@@ -8,8 +8,40 @@ import { Component } from "@angular/core";
 })
 export class EducationComponent {
 
-    constructor () {
+    edDisplayPod1: Boolean = true;
+    edDisplayPod2: Boolean = true;
+    edDisplayPod3: Boolean = true;
+    edDisplayPod4: Boolean = true;
+    constructor () { }
 
-
-    }
+    onMobileViewEd() {
+        let _onMobileViewEd: Boolean = true;
+        if (window.innerWidth <= 768) {
+          _onMobileViewEd = false;
+        }
+        // console.log(window.innerWidth);
+        return _onMobileViewEd;
+    
+      }
+    
+      togglePodEd(_EDpodNumber: number) {
+    
+        switch (_EDpodNumber) {
+          case 1:
+            this.edDisplayPod1 = !this.edDisplayPod1;
+            break;
+          case 2:
+            this.edDisplayPod2 = !this.edDisplayPod2;
+            break;
+          case 3:
+            this.edDisplayPod3 = !this.edDisplayPod3;
+            break;
+          case 4:
+            this.edDisplayPod4 = !this.edDisplayPod4;
+            break;
+          default:
+            break;
+        }
+    
+      }
 }

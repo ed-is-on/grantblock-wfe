dcb137538e311399eac9f9dbed6c3f97266285ad
Bryan Tanifum (WinXDev4-Ubuntu) <bryan.tanifum@gmail.com>
2018-09-03 23:44:09 -0400

    Created azure environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,11 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  azureStorage:{
+    name:'edhyperledger',
+    baseUrl:'blob.core.windows.net',
+    sas: '' ,
+    containerName:'receipts'
+  }
 };

3f57134e03a2a8a952c72407f330c945617bd2ff
Bryan Tanifum (WinXDev4-Ubuntu) <bryan.tanifum@gmail.com>
2018-09-18 19:04:07 -0400

    refactored code

--- src/components/education-review/education-review.component.ts
+++ src/components/education-review/education-review.component.ts
@@ -6,28 +6,29 @@ import { GrantBlockService } from '../../services/grantblock.service';
 @Component({
   selector: 'education-review',
   templateUrl: './education-review.component.html',
-  styleUrls: ['./education-review.component.css','../../app/app.component.css']
+  styleUrls: ['./education-review.component.css', '../../app/app.component.css']
 })
 export class EducationReviewComponent implements OnInit {
 
   blockchain: Transactions[];
   errorMessage: string;
-  pageInfo = {Title: 'Review Transactions', Description: 'Dept. of Education employees can reiew a list of transactions made by granteess'} 
-  matTableDataSource: MatTableDataSource<Transactions> = new MatTableDataSource<Transactions>();
-  displayedColumns: string[] = [ 'granteeId', 'date', 'amount', 'type', 'status', 'approvers'];
+  pageInfo = { Title: 'Review Transactions', Description: 'Dept. of Education employees can review a list of transactions made by granteess' }
   
+  matTableDataSource: MatTableDataSource<Transactions> = new MatTableDataSource<Transactions>();
+  displayedColumns: string[] = ['granteeId', 'date', 'amount', 'type', 'status', 'approvers'];
+
   constructor(
     private $grantblockService: GrantBlockService
   ) { }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort : MatSort;
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
     this.GetTransactions();
   }
 
-  applyFilter(filterValue:string){
+  applyFilter(filterValue: string) {
     this.matTableDataSource.filter = filterValue.trim().toLowerCase();
   }
 
@@ -40,14 +41,14 @@ export class EducationReviewComponent implements OnInit {
       this.matTableDataSource.paginator = this.paginator;
       this.matTableDataSource.sort = this.sort;
       // this.matTableDataSource.filterPredicate = (data:Transactions,filter:string)=>{
-        
+
       //   return true
       // }
-    }, 
-    (error) => {
-      this.blockchain = undefined;
-      this.errorMessage = error.message;
-    })
+    },
+      (error) => {
+        this.blockchain = undefined;
+        this.errorMessage = error.message;
+      })
   }
 
 }

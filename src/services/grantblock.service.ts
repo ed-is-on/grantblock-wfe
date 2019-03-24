ac5ca086b08547639fa938d95cf693c2d41bc02e
Bryan Tanifum <bryan.tanifum@gmail.com>
2019-03-24 18:13:39 -0400

    updated transactions view to display rejected transactions

--- src/services/grantblock.service.ts
+++ src/services/grantblock.service.ts
@@ -67,6 +67,8 @@ export class GrantBlockService {
                         } else if (_value.rejectValidators && _value.rejectValidators.length > 0 && _value.rejectValidators.map(x => { return x.userId }).indexOf(_approver.userId) > -1) {
                             approvalStatus = enumApprovalStatus.Rejected;
                         }
+                    }else{
+                        approvalStatus = enumApprovalStatus.Rejected;
                     }
 
                     transactionApprovers.push(new TransactionApprover(_approver.userId, _value.requestId, transaction.date, transaction.amount.toString(), approvalStatus, transaction.status, transaction.receiptImage, transaction.receiptHash, transaction.purpose))
